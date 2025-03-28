fbf00dfd071f6a74f3f1874deb11b4baadba36ce
ivanb6607 <95305522+ivanb6607@users.noreply.github.com>
2025-03-28 16:06:21 -0500

    octavo commit

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgres://postgres.lolamzuokwevbtjjfjsr:4a7DinSowBtXP6kg@aws-0-us-east-1.pooler.supabase.com:6543/verceldb?sslmode=require')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgres://postgres.lolamzuokwevbtjjfjsr:4a7DinSowBtXP6kg@aws-0-us-east-1.pooler.supabase.com:6543/postgres?sslmode=require&supa=base-pooler.x')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.urandom(24)  # or another method to set a secret key
